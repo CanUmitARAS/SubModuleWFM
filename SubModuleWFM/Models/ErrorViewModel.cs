04313a2e14945c85759b52852f819984dda1e176
ümit aras <umit.aras96@gmail.com>
2024-04-29 18:40:52 +0400

    Add project files.

---
+++ SubModuleWFM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SubModuleWFM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
